8d3f59e22b4e4618938d8e61c8c9dfc11f771ea2
HarryRus24 <harrison.rusdale@gmail.com>
2021-12-17 01:51:20 +0000

    Remove chess scripts

--- src/ServerScriptService/LoadModules.server.lua
+++ src/ServerScriptService/LoadModules.server.lua
@@ -1,11 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
 
 local require = table.unpack(require(ReplicatedStorage.ZenithFramework))
 
 require:LoadAll()
-
-local ChessHandler = require("ChessHandler")
-
-task.wait(10)
-ChessHandler.newGame(Players:WaitForChild("Player1"), Players:WaitForChild("Player2"), workspace.ChessBoard)
