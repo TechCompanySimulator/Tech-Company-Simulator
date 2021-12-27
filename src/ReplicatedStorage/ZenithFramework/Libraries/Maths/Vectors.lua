0acf978add41e5f4133fab9686826f1a3a77163d
Arthur Morton <56565952+TheM0rt0nator@users.noreply.github.com>
2021-12-27 21:52:05 +0000

    Vector library updates

--- src/ReplicatedStorage/ZenithFramework/Libraries/Maths/Vectors.lua
+++ src/ReplicatedStorage/ZenithFramework/Libraries/Maths/Vectors.lua
@@ -2,6 +2,8 @@ local Vectors = {}
 
 -- Converts a Vector to a string in the format 'x:y' or 'x:y:Z'
 function Vectors.vectorToString(vector)
+	assert(typeof(vector) == "Vector2" or typeof(vector) == "Vector3", "Vector arguments needs to be a Vector2 or a Vector3")
+
 	local endPart = ""
 	if typeof(vector) == "Vector3" then
 		endPart = ":" .. vector.z
@@ -12,6 +14,8 @@ end
 
 -- Converts a string in the format 'x:y' or 'x:y:z' to a Vector
 function Vectors.stringToVector(str)
+	assert(typeof(str) == "string", "String arguments needs to be a string")
+
 	local vals = str:split(":")
 	if #vals == 2 then
 		return Vector2.new(tonumber(vals[1]), tonumber(vals[2]))
@@ -20,8 +24,16 @@ function Vectors.stringToVector(str)
 	end
 end
 
--- Checks whether the given vector lies within the given bounds
+-- Checks whether the given vector lies within the given bounds (only works for Vector2 for now)
 function Vectors.checkWithinBounds(vector, lowerXBound, upperXBound, lowerYBound, upperYBound)
+	assert(typeof(vector) == "Vector2", "Vector arguments needs to be a Vector2")
+	assert(typeof(lowerXBound) == "number" 
+		and typeof(upperXBound) == "number" 
+		and typeof(lowerYBound) == "number" 
+		and typeof(upperYBound) == "number", 
+		"All bound arguments need to be numbers"
+	)
+
 	if vector.X >= lowerXBound
 		and vector.X <= upperXBound
 		and vector.Y >= lowerYBound
