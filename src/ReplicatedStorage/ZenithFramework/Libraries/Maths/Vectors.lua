0cd9a3566a7dc21fde9f32896f248f3a3ab48c77
Arthur Morton <56565952+TheM0rt0nator@users.noreply.github.com>
2021-12-27 22:28:59 +0000

    Vectors assertions

--- src/ReplicatedStorage/ZenithFramework/Libraries/Maths/Vectors.lua
+++ src/ReplicatedStorage/ZenithFramework/Libraries/Maths/Vectors.lua
@@ -2,7 +2,7 @@ local Vectors = {}
 
 -- Converts a Vector to a string in the format 'x:y' or 'x:y:Z'
 function Vectors.vectorToString(vector)
-	assert(typeof(vector) == "Vector2" or typeof(vector) == "Vector3", "Vector arguments needs to be a Vector2 or a Vector3")
+	assert(typeof(vector) == "Vector2" or typeof(vector) == "Vector3", "Vector arguments need to be a Vector2 or a Vector3")
 
 	local endPart = ""
 	if typeof(vector) == "Vector3" then
@@ -26,7 +26,7 @@ end
 
 -- Checks whether the given vector lies within the given bounds (only works for Vector2 for now)
 function Vectors.checkWithinBounds(vector, lowerXBound, upperXBound, lowerYBound, upperYBound)
-	assert(typeof(vector) == "Vector2", "Vector arguments needs to be a Vector2")
+	assert(typeof(vector) == "Vector2", "Vector argument needs to be a Vector2")
 	assert(typeof(lowerXBound) == "number" 
 		and typeof(upperXBound) == "number" 
 		and typeof(lowerYBound) == "number" 
