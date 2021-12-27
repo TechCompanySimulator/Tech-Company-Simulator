005051922413eeb22193a7f6a8c1ea6b04412fe8
Arthur Morton <56565952+TheM0rt0nator@users.noreply.github.com>
2021-12-27 17:44:52 +0000

    Added all tests complete notification

--- src/ReplicatedStorage/SharedScripts/TestHandler.lua
+++ src/ReplicatedStorage/SharedScripts/TestHandler.lua
@@ -9,9 +9,13 @@ local loadModule = table.unpack(require(Zenith))
 local TestEZ = loadModule("TestEZ")
 local CONFIG = loadModule("CONFIG")
 
+local RanTests = false
+
+local area = (RunService:IsServer() and "server") or "client"
+
 if CONFIG.RUN_FRAMEWORK_TESTS then
+	RanTests = true
 	local tests = (RunService:IsServer() and Zenith.Tests.Server) or Zenith.Tests.Client
-	local area = (RunService:IsServer() and "server") or "client"
 	warn("Framework shared tests started")
 	local sharedResults = TestEZ.TestBootstrap:run(Zenith.Tests.Shared:GetChildren())
 	warn("Framework shared tests complete")
@@ -22,21 +26,28 @@ if CONFIG.RUN_FRAMEWORK_TESTS then
 end
 
 if CONFIG.RUN_SHARED_TESTS then
+	RanTests = true
 	warn("Shared tests started")
 	local results = TestEZ.TestBootstrap:run(ReplicatedStorage.SharedTests:GetChildren())
 	warn("Shared tests Complete")
 end
 
 if CONFIG.RUN_SERVER_TESTS and RunService:IsServer() then
+	RanTests = true
 	warn("Server tests started")
 	local results = TestEZ.TestBootstrap:run(game.ServerScriptService.ServerTests:GetChildren())
 	warn("Server tests Complete")
 end
 
 if CONFIG.RUN_CLIENT_TESTS and RunService:IsClient() then
+	RanTests = true
 	warn("Client tests started")
 	local results = TestEZ.TestBootstrap:run(Players.LocalPlayer.PlayerScripts.ClientTests:GetChildren())
 	warn("Client tests Complete")
+end
+
+if RanTests then
+	warn("All " .. area .. " tests complete")
 end
 
 return {}
