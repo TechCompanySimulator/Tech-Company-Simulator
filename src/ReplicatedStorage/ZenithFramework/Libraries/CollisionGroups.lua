5bf37ef10b6e06878b265becab7a2625d389e5fa
Arthur Morton <56565952+TheM0rt0nator@users.noreply.github.com>
2021-12-28 21:15:29 +0000

    Collision group checks

--- src/ReplicatedStorage/ZenithFramework/Libraries/CollisionGroups.lua
+++ src/ReplicatedStorage/ZenithFramework/Libraries/CollisionGroups.lua
@@ -14,6 +14,8 @@ local DEFAULT_GROUPS = {
 
 -- Creates a new collision group
 function CollisionGroups.newCollisionGroup(groupName)
+	assert(typeof(groupName) == "string", "Group name needs to be a string")
+
 	if not CollisionGroups.groupExists(groupName) then
 		PhysicsService:CreateCollisionGroup(groupName)
 	end
@@ -21,6 +23,8 @@ end
 
 -- Checks if the collision group with the given name exists
 function CollisionGroups.groupExists(groupName)
+	assert(typeof(groupName) == "string", "Group name needs to be a string")
+
 	for _, group in pairs(PhysicsService:GetCollisionGroups()) do
 		if group and group.name == groupName then
 			return true
@@ -31,6 +35,9 @@ end
 -- Assigns a collision group to an instance or model (if model, assigns to any added children too)
 -- Creates the collision group if it doesn't exist
 function CollisionGroups.assignGroup(obj, groupName)
+	assert(obj:IsA("BasePart") or obj:IsA("Model"), "Object argument needs to be a BasePart or a Model")
+	assert(typeof(groupName) == "string", "Group name needs to be a string")
+
 	if not CollisionGroups.groupExists(groupName) then
 		CollisionGroups.newCollisionGroup(groupName)
 	end
@@ -54,6 +61,9 @@ end
 
 -- Sets whether 2 collision groups should be collidable or not
 function CollisionGroups.setGroupsCollidable(group1, group2, bool)
+	assert(typeof(group1) == "string" and typeof(group2) == "string", "Group arguments need to be strings")
+	assert(typeof(bool) == "boolean", "Bool argument needs to be a boolean")
+
 	if CollisionGroups.groupExists(group1) and CollisionGroups.groupExists(group2) then
 		PhysicsService:CollisionGroupSetCollidable(group1, group2, bool)
 	end
