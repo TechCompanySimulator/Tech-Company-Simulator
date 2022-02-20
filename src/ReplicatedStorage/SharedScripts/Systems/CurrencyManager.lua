20b0c06c9c1768c078a6ed63d7d073a72023490a
Arthur Morton <56565952+TheM0rt0nator@users.noreply.github.com>
2022-02-20 23:09:34 +0000

    Made transactions only available on server

--- src/ReplicatedStorage/SharedScripts/Systems/CurrencyManager.lua
+++ src/ReplicatedStorage/SharedScripts/Systems/CurrencyManager.lua
@@ -1,4 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 
 local loadModule = table.unpack(require(ReplicatedStorage.ZenithFramework))
 
@@ -10,19 +11,21 @@ local CurrencyManager = {}
 
 -- Adds the amount of currency the players currency saved in their data
 function CurrencyManager:transact(player, currency, amount)
-	local playerData = RoduxStore:waitForValue("playerData", tostring(player.UserId))
-	local currentAmount = playerData[currency] or 0
-	local canTransact = amount >= 0 or CurrencyManager.hasEnoughCurrency(player, currency, amount)
-	if canTransact then
-		RoduxStore:dispatch(setPlayerData(player.UserId, currency, currentAmount + amount))
-		return true
+	if RunService:IsServer() and typeof(currency) == "string" and typeof(amount) == "number" then
+		local playerData = RoduxStore:waitForValue("playerData", tostring(player.UserId))
+		local currentAmount = playerData[currency] or 0
+		local canTransact = amount >= 0 or CurrencyManager.hasEnoughCurrency(player, currency, amount)
+		if canTransact then
+			RoduxStore:dispatch(setPlayerData(player.UserId, currency, currentAmount + amount))
+			return true
+		end
 	end
 end
 
 -- Returns the amount of the given currency the player has
 function CurrencyManager.getCurrencyAmount(player, currency)
 	local playerData = RoduxStore:waitForValue("playerData", tostring(player.UserId))
-	return playerData[currency]
+	return typeof(currency) == "string" and playerData[currency]
 end
 
 -- Returns true or false depending on if they have enough of the given currency or not
