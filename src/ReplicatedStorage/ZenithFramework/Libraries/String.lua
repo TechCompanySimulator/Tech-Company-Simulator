2c819078301e14b9bf3fbd26a2ec6421e9795a90
Arthur Morton <56565952+TheM0rt0nator@users.noreply.github.com>
2022-03-06 01:01:28 +0000

    String commaFormat function with unit test

--- src/ReplicatedStorage/ZenithFramework/Libraries/String.lua
+++ src/ReplicatedStorage/ZenithFramework/Libraries/String.lua
@@ -100,4 +100,17 @@ function String.convertToHMS(seconds)
 	return hoursText .. ":" .. minutesText .. ":" .. secondsText
 end
 
+-- Converts a number to have commas in the correct places
+function String.commaFormat(amount)
+	local formatted = amount
+	local k
+	while true do  
+		formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+		if (k == 0) then
+			break
+		end
+	end
+	return formatted
+  end
+
 return String
