c7266f63f4c178be2a5329fd32044d779acfdc7c
Harrison Rusdale <harrison.rusdale@gmail.com>
2023-09-17 16:58:21 +0100

    Base machines game values

--- src/ReplicatedStorage/SharedScripts/GameValues/machines.lua
+++ src/ReplicatedStorage/SharedScripts/GameValues/machines.lua
@@ -85,4 +85,167 @@ return {
 			};
 		};
 	};
+	computers = {
+		machineCost = 5000; -- TODO: Correct Value
+		buildItems = {
+			"Genesis";
+			"Havoc";
+			"Maverick";
+			"Goliath";
+			"RoForce 1.0";
+			"Havoc";
+			"Hydra";
+			"RoForce 2.0";
+			"RoForce 4.0";
+		};
+		qualityUpgrades = {
+			[1] = {
+				cost = 400;
+				currency = "Coins";
+			};
+			[2] = {
+				cost = 800;
+				currency = "Coins";
+			};
+			[3] = {
+				cost = 1600;
+				currency = "Coins";
+			};
+			[4] = {
+				cost = 5000;
+				currency = "Coins";
+			};
+			[5] = {
+				cost = 15000;
+				currency = "Coins";
+			};
+			[6] = {
+				cost = 60000;
+				currency = "Coins";
+			};
+			[7] = {
+				cost = 100000;
+				currency = "Coins";
+			};
+			[8] = {
+				cost = 200000;
+				currency = "Coins";
+			};
+			[9] = {
+				cost = 500000;
+				currency = "Coins";
+			};
+			[10] = {
+				cost = 30;
+				currency = "Gems";
+			};
+			[11] = {
+				cost = 40;
+				currency = "Gems";
+			};
+			[12] = {
+				cost = 50;
+				currency = "Gems";
+			};
+			[13] = {
+				cost = 60;
+				currency = "Gems";
+			};
+		};
+		speedUpgrades = {
+			[1] = {
+				cost = 5000;
+				currency = "Coins";
+			};
+			[2] = {
+				cost = 10000;
+				currency = "Coins";
+			};
+			[3] = {
+				cost = 15000;
+				currency = "Coins";
+			};
+		};
+	};
+	accessories = {
+		machineCost = 5000; -- TODO: Correct Value
+		buildItems = {
+			"Basic Monitor";
+			"Flatscreen Monitor";
+			"HD Monitor";
+			"Widescreen Monitor";
+			"Hull HD Widescreen Monitor";
+			"RoForce 1.0";
+			"RoForce 2.0";
+		};
+		qualityUpgrades = {
+			[1] = {
+				cost = 400;
+				currency = "Coins";
+			};
+			[2] = {
+				cost = 800;
+				currency = "Coins";
+			};
+			[3] = {
+				cost = 1600;
+				currency = "Coins";
+			};
+			[4] = {
+				cost = 5000;
+				currency = "Coins";
+			};
+			[5] = {
+				cost = 15000;
+				currency = "Coins";
+			};
+			[6] = {
+				cost = 60000;
+				currency = "Coins";
+			};
+			[7] = {
+				cost = 100000;
+				currency = "Coins";
+			};
+			[8] = {
+				cost = 200000;
+				currency = "Coins";
+			};
+			[9] = {
+				cost = 500000;
+				currency = "Coins";
+			};
+			[10] = {
+				cost = 30;
+				currency = "Gems";
+			};
+			[11] = {
+				cost = 40;
+				currency = "Gems";
+			};
+			[12] = {
+				cost = 50;
+				currency = "Gems";
+			};
+			[13] = {
+				cost = 60;
+				currency = "Gems";
+			};
+		};
+		speedUpgrades = {
+			[1] = {
+				cost = 5000;
+				currency = "Coins";
+			};
+			[2] = {
+				cost = 10000;
+				currency = "Coins";
+			};
+			[3] = {
+				cost = 15000;
+				currency = "Coins";
+			};
+		};
+	};
+	-- TODO: Research costs in another table
 }
