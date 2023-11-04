b7d03f784f3e91b928f879712b56a2a31277eade
Harrison Rusdale <harrison.rusdale@gmail.com>
2023-11-04 14:36:07 +0000

    Base Research System

--- src/ReplicatedStorage/SharedScripts/Systems/ResearchSystem.lua
+++ src/ReplicatedStorage/SharedScripts/Systems/ResearchSystem.lua
@@ -3,9 +3,16 @@ local RunService = game:GetService("RunService")
 
 local loadModule, getDataStream = table.unpack(require(ReplicatedStorage.ZenithFramework))
 
+local CurrencyManager = loadModule("CurrencyManager")
 local Llama = loadModule("Llama")
 local PlayerDataManager = loadModule("PlayerDataManager")
 local RoduxStore = loadModule("RoduxStore")
+local MachineUtility
+
+local completeResearch = loadModule("completeResearch")
+local incrementResearchProgress = loadModule("incrementResearchProgress")
+
+local incrementResearchRemote = getDataStream("incrementResearch", "RemoteFunction")
 
 local ResearchSystem = {}
 
@@ -22,8 +29,75 @@ function ResearchSystem.getPlayerLevel(player : Player, machineType : string) : 
 	return playerLevel.Level, playerLevel.Progress
 end
 
+function ResearchSystem.getResearchCosts(machineType : string, level : number) : table?
+	local researchCostValues = RoduxStore:waitForValue("gameValues", "researchCosts")[string.lower(machineType)]
 
--- TODO: Set Player Level
+	if not researchCostValues then
+		warn("Research costs not found for machine type: " .. machineType)
+		return
+	end
 
+	local researchCosts = researchCostValues[level]
+
+	if not researchCostValues then
+		warn("Research costs not found for machine type: " .. machineType .. " at level: " .. level)
+		return
+	end
+
+	return researchCosts
+end
+
+function ResearchSystem.isResearchCompleted(player : Player, machineType : string) : boolean
+	local playerLevel, playerProgress = ResearchSystem.getPlayerLevel(player, machineType)
+	local researchCosts = ResearchSystem.getResearchCosts(machineType, playerLevel + 1)
+
+	return Llama.Dictionary.length(playerProgress) == #researchCosts
+end
+
+function ResearchSystem.incrementResearch(player : Player, machineType : string, researchIndex : number) : boolean?
+	MachineUtility = MachineUtility or loadModule("MachineUtility")
+
+	local playerLevel, playerProgress = ResearchSystem.getPlayerLevel(player, machineType)
+
+	if playerProgress[researchIndex] then
+		warn("Research already completed for player: " .. player.Name .. " for machine type: " .. machineType .. " with index: " .. researchIndex)
+		return
+	end
+
+	local researchCosts = ResearchSystem.getResearchCosts(machineType, playerLevel + 1)
+	if not researchCosts then return end
+
+	local priceInfo = researchCosts[researchIndex]
+
+	if not priceInfo then
+		warn("Research costs not found for machine type: " .. machineType .. " at level: " .. playerLevel + 1 .. " with index: " .. researchIndex)
+		return
+	end
+
+	local cost = if typeof(priceInfo) == "table" then priceInfo.cost else priceInfo
+	local currency = if typeof(priceInfo) == "table" then priceInfo.currency else "Coins"
+
+	if CurrencyManager:hasAmount(player, currency, cost) then
+		if RunService:IsServer() then
+			local success = CurrencyManager:transact(player, currency, -cost)
+
+			if success then
+				local machineDisplayName = MachineUtility.getMachineDisplayName(machineType)
+
+				-- Increment the player researchProgress
+				PlayerDataManager:updatePlayerData(player, incrementResearchProgress, machineDisplayName, researchIndex)
+
+				-- If they have done all research steps then increment their research level
+				if ResearchSystem.isResearchCompleted(player, machineType) then
+					PlayerDataManager:updatePlayerData(player, completeResearch, machineDisplayName, playerLevel + 1)
+				end
+			end
+
+			return success
+		else
+			return incrementResearchRemote:InvokeServer(machineType, researchIndex)
+		end
+	end
+end
 
 return ResearchSystem
