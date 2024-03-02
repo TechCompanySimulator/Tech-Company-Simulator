2989c263bcf34fee0ce07e9dd3f89b0d811e2574
Harrison Rusdale <harrison.rusdale@gmail.com>
2024-03-02 18:04:07 +0000

    Update ResearchSystem.lua

--- src/ReplicatedStorage/SharedScripts/Systems/ResearchSystem.lua
+++ src/ReplicatedStorage/SharedScripts/Systems/ResearchSystem.lua
@@ -6,7 +6,6 @@ local loadModule, getDataStream = table.unpack(require(ReplicatedStorage.ZenithF
 local CurrencyManager = loadModule("CurrencyManager")
 local Llama = loadModule("Llama")
 local RoduxStore = loadModule("RoduxStore")
-local MachineUtility = loadModule("MachineUtility")
 local PlayerDataManager = RunService:IsServer() and loadModule("PlayerDataManager")
 
 local completeResearch = loadModule("completeResearch")
@@ -16,7 +15,7 @@ local incrementResearchRemote = getDataStream("incrementResearch", "RemoteFuncti
 
 local ResearchSystem = {}
 
-function ResearchSystem.getPlayerLevel(player : Player, machineType : string) : (number, table)
+function ResearchSystem.getPlayerLevel(player: Player, machineType: string): (number, table)
 	local playerData = RoduxStore:waitForValue("playerData")[tostring(player.UserId)] or {}
 	local researchLevels = playerData.ResearchLevels
 
@@ -27,13 +26,13 @@ function ResearchSystem.getPlayerLevel(player : Player, machineType : string) : 
 	return playerLevel.Level, playerLevel.Progress
 end
 
-function ResearchSystem.hasPlayerResearched(player : Player, machineType : string, level : number) : boolean
+function ResearchSystem.hasPlayerResearched(player: Player, machineType: string, level: number): boolean
 	local playerLevel, _ = ResearchSystem.getPlayerLevel(player, machineType)
 
 	return playerLevel >= level
 end
 
-function ResearchSystem.getResearchCosts(machineType : string, level : number) : table?
+function ResearchSystem.getResearchCosts(machineType: string, level: number): table?
 	local researchCostValues = RoduxStore:waitForValue("gameValues", "researchCosts")[string.lower(machineType)]
 
 	if not researchCostValues then
@@ -51,14 +50,24 @@ function ResearchSystem.getResearchCosts(machineType : string, level : number) :
 	return researchCosts
 end
 
-function ResearchSystem.isResearchCompleted(player : Player, machineType : string) : boolean
+function ResearchSystem.isResearchCompleted(player: Player, machineType: string): boolean
 	local playerLevel, playerProgress = ResearchSystem.getPlayerLevel(player, machineType)
 	local researchCosts = ResearchSystem.getResearchCosts(machineType, playerLevel + 1)
 
 	return Llama.Dictionary.length(playerProgress) == #researchCosts
 end
 
-function ResearchSystem.incrementResearch(player : Player, machineType : string, researchIndex : number) : boolean?
+function ResearchSystem.incrementResearch(player: Player, machineType: string, researchIndex: number): boolean?
+	if not typeof(researchIndex) == "number" then
+		warn("Invalid research index: " .. researchIndex)
+		return false
+	end
+
+	if not typeof(machineType) == "string" then
+		warn("Invalid machine type: " .. machineType)
+		return false
+	end
+
 	local playerLevel, playerProgress = ResearchSystem.getPlayerLevel(player, machineType)
 
 	if playerProgress[researchIndex] then
@@ -89,6 +98,7 @@ function ResearchSystem.incrementResearch(player : Player, machineType : string,
 
 				-- If they have done all research steps then increment their research level
 				if ResearchSystem.isResearchCompleted(player, machineType) then
+					print("COMPLETE RESEARCH")
 					PlayerDataManager:updatePlayerData(player, completeResearch, machineType, playerLevel + 1)
 				end
 			end
@@ -98,6 +108,36 @@ function ResearchSystem.incrementResearch(player : Player, machineType : string,
 			return incrementResearchRemote:InvokeServer(machineType, researchIndex)
 		end
 	end
+end
+
+function ResearchSystem.getFirstUIDisplayType(player: Player): string
+	local machineData = RoduxStore:waitForValue("gameValues", "machines")
+
+	local playerData = RoduxStore:waitForValue("playerData")[tostring(player.UserId)] or {}
+	local researchLevels = playerData.ResearchLevels or {}
+
+	local baseDisplayOrder = math.huge
+	local selectedResearchType = "phone"
+
+	for researchType, researchData in researchLevels do
+		local level = researchData.Level
+
+		local currentMachineData = machineData[researchType]
+
+		local buildOptions = currentMachineData.buildItems
+
+		-- If the player has not fully researched this item and it is the lowest display order then set it as the selectedResearchType
+		if buildOptions[level + 1] and currentMachineData.displayOrder <= baseDisplayOrder then
+			baseDisplayOrder = currentMachineData.displayOrder
+			selectedResearchType = researchType
+		end
+	end
+
+	return selectedResearchType
+end
+
+if RunService:IsServer() then
+	incrementResearchRemote.OnServerInvoke = ResearchSystem.incrementResearch
 end
 
 return ResearchSystem
