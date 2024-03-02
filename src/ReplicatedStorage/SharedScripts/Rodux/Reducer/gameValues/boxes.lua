763a37104163c0e2e6fc95e605ae85c72333c9a7
Harrison Rusdale <harrison.rusdale@gmail.com>
2024-03-02 17:46:33 +0000

    Machines and Boxes GameValues

---
+++ src/ReplicatedStorage/SharedScripts/Rodux/Reducer/gameValues/boxes.lua
@@ -0,0 +1,47 @@
+return {
+	[1] = {
+		cost = 0;
+		currency = "Coins";
+		capacity = 5;
+	};
+	[2] = {
+		cost = 300;
+		currency = "Coins";
+		capacity = 10;
+	};
+	[3] = {
+		cost = 2000;
+		currency = "Coins";
+		capacity = 25;
+	};
+	[4] = {
+		cost = 5000;
+		currency = "Coins";
+		capacity = 50;
+	};
+	[5] = {
+		cost = 10000;
+		currency = "Coins";
+		capacity = 100;
+	};
+	[6] = {
+		cost = 25000;
+		currency = "Coins";
+		capacity = 200;
+	};
+	[7] = {
+		cost = 50000;
+		currency = "Coins";
+		capacity = 250;
+	};
+	[8] = {
+		cost = 100000;
+		currency = "Coins";
+		capacity = 500;
+	};
+	[9] = {
+		cost = 250000;
+		currency = "Coins";
+		capacity = 700;
+	}
+}
