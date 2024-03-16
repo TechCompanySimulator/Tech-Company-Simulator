ef46cfc123d0109239f373181d10bfdd311b21b7
Harrison Rusdale <harrison.rusdale@gmail.com>
2024-03-16 15:06:53 +0000

    Update MachineUtility.lua

--- src/ReplicatedStorage/SharedScripts/Systems/MachineUtility.lua
+++ src/ReplicatedStorage/SharedScripts/Systems/MachineUtility.lua
@@ -4,23 +4,42 @@ local loadModule = table.unpack(require(ReplicatedStorage.ZenithFramework))
 
 local RoduxStore = loadModule("RoduxStore")
 
+local VALID_LEVEL_TYPES = {
+	"speed";
+	"quality";
+}
+
 local MachineUtility = {}
 
--- TODO: Function to upgrade machine looks to match levels, with machineModel as argument
-
-function MachineUtility.getMachineValues(machineType: string)
-	return RoduxStore:waitForValue("gameValues", "machines")[machineType:lower()]
+function MachineUtility.isValidLevelType(levelType: string): boolean
+	return table.find(VALID_LEVEL_TYPES, levelType) ~= nil
 end
 
-function MachineUtility.getBuildItems(machineType: string)
-	local machineValues = MachineUtility.getMachineValues(machineType)
+function MachineUtility.getMachineValues(machineType: string): table?
+	local machineValues = RoduxStore:waitForValue("gameValues", "machines")[machineType:lower()]
 
 	if not machineValues then
 		warn("Machine values not found for machine type: " .. machineType)
 		return
 	end
 
-	return machineValues.buildItems
+	return machineValues
 end
+
+function MachineUtility.getBuildItems(machineType: string): table?
+	local machineValues = MachineUtility.getMachineValues(machineType)
+
+	return if machineValues then machineValues.buildItems else nil
+end
+
+function MachineUtility.getUpgradeValues(machineType: string, levelType: string): table?
+	if not MachineUtility.isValidLevelType(levelType) then return end
+
+	local machineValues = MachineUtility.getMachineValues(machineType)
+
+	return if machineValues then machineValues[levelType .. "Upgrades"] else nil
+end
+
+-- TODO: Function to upgrade machine looks to match levels, with machineModel as argument
 
 return MachineUtility
