5c45563ab89f338ac3e2dca23dbe09d9df00668e
Harrison Rusdale <harrison.rusdale@gmail.com>
2024-03-24 17:22:24 +0000

    Update Maid.lua

--- src/ReplicatedStorage/ZenithFramework/Libraries/Maid.lua
+++ src/ReplicatedStorage/ZenithFramework/Libraries/Maid.lua
@@ -4,6 +4,20 @@
 local Maid = {}
 Maid.__index = Maid
 
+local function disconnectTask(cleanupTask)
+	if typeof(cleanupTask) == "RBXScriptConnection" then
+		cleanupTask:Disconnect()
+	elseif typeof(cleanupTask) == "function" then
+		cleanupTask()
+	elseif typeof(cleanupTask) == "thread" then
+		task.cancel(cleanupTask)
+	elseif typeof(cleanupTask) == "Instance" then
+		cleanupTask:Destroy()
+	elseif typeof(cleanupTask) == "table" and typeof(cleanupTask.destroy) == "function" then
+		cleanupTask:destroy()
+	end
+end
+
 -- Creates a new maid object
 function Maid.new()
 	local self = setmetatable({}, Maid)
@@ -14,18 +28,27 @@ function Maid.new()
 end
 
 -- Stores a task in the maid
-function Maid:GiveTask(task)
-	table.insert(self.connections, task)
+function Maid:GiveTask(_task)
+	table.insert(self.connections, _task)
 end
 
 -- Clears all tasks stored in the maid
 function Maid:DoCleaning()
-	for _, task in pairs(self.connections) do
-		if typeof(task) == "RBXScriptConnection" then
-			task:Disconnect()
-		end
+	for index, cleanupTask in self.connections do
+		if typeof(cleanupTask) ~= "RBXScriptConnection" then continue end
+
+		self.connections[index] = nil
+		disconnectTask(cleanupTask)
 	end
-	self.connections = {}
+
+	local key, cleanupTask = next(self.connections)
+
+	while cleanupTask ~= nil do
+		self.connections[key] = nil
+		disconnectTask(cleanupTask)
+
+		key, cleanupTask = next(cleanupTask)
+	end
 end
 
 Maid.giveTask = Maid.GiveTask
